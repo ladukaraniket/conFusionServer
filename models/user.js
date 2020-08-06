7d0f7ed2678ba848ec4b9e16deb5d2c0d8d729cd
Aniket Ladukar <ladukaraniket@gmail.com>
2020-08-06 12:48:19 +0530

    User Authentication with Passport

--- models/user.js
+++ models/user.js
@@ -1,22 +1,14 @@
 const mongoose = require('mongoose');
-
 const Schema = mongoose.Schema;
+const passportLocalMongoose = require('passport-local-mongoose');
 
 const userSchema = new Schema({
-    username: {
-        type: String,
-        required: true,
-        unique: true
-    },
-    password: {
-        type: String,
-        required: true,
-    },
     admin: {
         type: Boolean,
         default: false
     }
 });
+userSchema.plugin(passportLocalMongoose);
 
 var User = mongoose.model('User', userSchema);
 
